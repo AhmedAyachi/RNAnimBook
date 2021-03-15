01a2ab10e04c2a2775d7db6060a1f2b80daab76b
AhmedAyachi <aayachi032@gmail.com>
2021-03-15 08:31:03 +0100

    to add expo-font

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from "react";
+import {Platform,UIManager} from "react-native";
+import {NativeRouter} from "react-router-native";
 import {registerRootComponent} from "expo";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import Reducer from "./Store";
 import App from "./App";
+import {useCustomFonts} from "./index.style";
 
 
 const store=createStore(Reducer,composeWithDevTools());
+if(Platform.OS==="android"&&UIManager.setLayoutAnimationEnabledExperimental){
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
-registerRootComponent(()=>(
-    <Provider store={store}>
-        <App/>
-    </Provider>
-));
+registerRootComponent(()=>{
+    const fontsloaded=useCustomFonts();
+    return (fontsloaded&&
+        <Provider store={store}>
+            <NativeRouter>
+                <App/>
+            </NativeRouter>
+        </Provider>
+    )
+});
--- src/index.style.js
+++ src/index.style.js
@@ -1,7 +1,21 @@
 import {Dimensions} from "react-native";
+import {useFonts} from "expo-font";
+import {princesssofia,anton,cabin,squadaone,roboto} from "assets";
 
 
-const {height,width}=Dimensions.get("screen");
+export const useCustomFonts=()=>{
+    const [loaded]=useFonts({
+        princesssofia,anton,cabin,squadaone,roboto
+    });
+    return loaded;
+}
+
+export const {height,width}=Dimensions.get("screen");
 export const vw=width*0.01;
 export const vh=height*0.01;
 export const rem=3*vw;
+
+export const border=(width=2,style="solid",color="black")=>({borderWidth:width,borderStyle:style,borderColor:color});
+export const rotate=(x="0deg",y="0deg",z="0deg")=>[{rotateX:x},{rotateY:y},{rotateZ:z}];
+export const skew=(x="0deg",y="0deg")=>[{skewX:x},{skewY:y}];
+export const inset=(value=0)=>({top:value,right:value,bottom:value,left:value});
