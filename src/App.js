df74fa00f4280c2169d3f5546c09d10f43d429b2
AhmedAyachi <aayachi032@gmail.com>
2024-02-06 22:58:19 +0100

    update expo sdk to v50

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from "react";
 import {StatusBar} from "expo-status-bar";
-import Navigator from "./Navigator/Navigator";
+import {NavigationContainer} from "@react-navigation/native";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import {AboutScreen,ComponentScreen,HomeScreen} from "screens";
+import * as components from "components";
+import {backgroundColor,mainColor} from "stylesheet";
 
 
 export default function App(){
     return <>
         <StatusBar translucent style="auto"/>
-        <Navigator/>
-    </>;
+        <NavigationContainer>
+            <DrawerNavigator screenOptions={getOptions} initialRouteName="home">
+                <Screen name="home" component={HomeScreen}/>
+                {Object.keys(components).map(key=>(
+                    <Screen 
+                        key={key} name={key} 
+                        children={()=><ComponentScreen name={key.toLowerCase()} component={components[key]}/>}
+                    />
+                ))}
+                <Screen name="about" component={AboutScreen}/>
+            </DrawerNavigator>
+        </NavigationContainer>
+    </>
 }
+
+const {Navigator:DrawerNavigator,Screen}=createDrawerNavigator();
+const getOptions=({route:{name}})=>{
+    const title=`${name[0].toUpperCase()}${name.substring(1)}`;
+    return {
+        title,
+        headerTitle:title,
+        headerStyle:{
+            //backgroundColor,
+        },
+        drawerActiveTintColor:mainColor,
+    };
+};
